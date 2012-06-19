a616e5c24704534c282266dc47de5c1c72afc3d1
adaugelli <adaugelli@gmail.com>
2012-06-18 22:55:22 -0700

    Update master

--- apt.php
+++ apt.php
@@ -5,6 +5,6 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 <Response>
   <Gather action="http://apt.example.com/gather.php" method="POST">
-    <Say voice="woman">Welcome! If you'd like to call Sean press 1. If you'd like Pamela press 2.</Say>
+    <Say voice="woman">Welcome! If you'd like to call Adam press 1. If you'd like Bryan press 2.</Say>
   </Gather>
 </Response>
